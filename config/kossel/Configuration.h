6bd6f30a7a6be1203c35c23762d8c674547be058
Yousry Abdallah <yousrya@gmail.com>
2020-10-24 09:09:19 +0200

    Fix board version.

--- config/kossel/Configuration.h
+++ config/kossel/Configuration.h
@@ -145,7 +145,7 @@
 
 // Choose the name from boards.h that matches your setup
 #ifndef MOTHERBOARD
-  #define MOTHERBOARD BOARD_TRIGORILLA_14
+  #define MOTHERBOARD BOARD_TRIGORILLA_14_11
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
